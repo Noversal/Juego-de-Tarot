5b6cecc54a775178c7b227578f982430d9f946a3
Noversal <dionofrio_franco@hotmail.com>
2022-08-01 00:38:16 -0300

    Arreglando que lea el script

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,6 @@ const path = require('path')
 module.exports = {
   mode:'production',
   entry:'./public/index.js',
-  output: {
-    filename: 'bundle.js',
-    path:path.resolve(__dirname)
-  },
   module: {
     
     rules:[
