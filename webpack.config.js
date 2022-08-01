d36afb036e6b1efe01bdb14a5482d4643817aeac
Noversal <dionofrio_franco@hotmail.com>
2022-08-01 00:34:15 -0300

    Segundo Intento de correjir el bundle

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry:'./public/index.js',
   output: {
     filename: 'bundle.js',
-    path:path.resolve(__dirname,'build')
+    path:path.resolve(__dirname)
   },
   module: {
     
